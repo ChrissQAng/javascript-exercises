0db5ce9f89557f704dbddba7faf935ba0a95f37a
ChrissQAng <132443487+ChrissQAng@users.noreply.github.com>
2023-07-26 13:42:40 +0200

    puhh not easy but i made it git add .!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newStr = "";
+    let selChar = "";
+    for (let i = 0; i <= string.length; i++) {
+        selChar = string.slice(string.length -1-i, string.length - i);
+        newStr += selChar;
+    }
+return newStr;
 };
 
 // Do not edit below this line
